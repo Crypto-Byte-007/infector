4e58da2a39cab80b35b4a148ae39e69bc37f10d6
Crypto-Byte-007 <arumugapandik1357@gmail.com>
2026-02-06 12:46:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def shell():
     return "FLAG: NULLCTF{Upl0oad_F1lters_are_A_1ie}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
